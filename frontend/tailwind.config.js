d513db872b4463e727294add8a2a627d6ee1cb96
anaj00 <bantolino.jana@gmail.com>
2024-02-10 14:41:26 +0800

    chore: updates dependencies

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                'primary-blue': '#4265D6',
+                'secondary-yellow': '#EFBE69',
+                'accent1-neon-blue': '#78F1F1',
+                'accent2-indigo': '#293855',
+                'accent3-purple': '#8E54E9',
+                'white': '#FFFFFF',
+                'dirty-white': '#F4F7F9',
+                'off-white': '#BABABA',
+                'black': '#1E1E1E',
+                'grey': '#404040'
+            }
+        }
     },
     plugins: []
 }
