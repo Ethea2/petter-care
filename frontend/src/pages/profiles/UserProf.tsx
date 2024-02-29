c138f43fad0555986a539bb711f7c0c648d871b9
Paula Pacheco <100471162+ennxxx@users.noreply.github.com>
2024-02-29 12:21:03 +0800

    fix: width of posts in profile

--- frontend/src/pages/profiles/UserProf.tsx
+++ frontend/src/pages/profiles/UserProf.tsx
@@ -14,6 +14,7 @@ const UserProf = () => {
                     <CreatePost />
                     <ProfileFilter />
                     <Post />
+                    <Post />
                 </div>
             </div>
         </>
