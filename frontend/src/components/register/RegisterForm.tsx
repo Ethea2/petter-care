959c1b0c1d33953698ccbc2dea44db629fad1ffa
Jana Marie Bantolino <102508350+anaj00@users.noreply.github.com>
2024-03-01 13:39:36 +0800

    chore: fixed login + register

--- frontend/src/components/register/RegisterForm.tsx
+++ frontend/src/components/register/RegisterForm.tsx
@@ -1,5 +1,6 @@
 import {
     TextInput,
+    PasswordInput,
     Checkbox,
     Anchor,
     Paper,
@@ -8,7 +9,6 @@ import {
     UnstyledButton,
     Group,
     Button,
-    PasswordInput,
 } from "@mantine/core"
 import { useState } from "react"
 
@@ -17,7 +17,7 @@ import useRegister from "../../hooks/useRegister"
 import { toast } from "react-toastify"
 
 const RegisterForm = () => {
-    const [username, setUsername] = useState<string>()
+const [username, setUsername] = useState<string>()
     const [password, setPassowrd] = useState<string>()
     const [confirm, setConfirm] = useState<string>()
     const [email, setEmail] = useState<string>()
@@ -45,91 +45,80 @@ const RegisterForm = () => {
 
     return (
         <>
-            <Paper className="h-full p-20 px-40" shadow="lg" radius="lg">
-                <img
-                    className="mx-auto pr-4 mb-12 w-2/3 hover:brightness-75 cursor-pointer transition duration-400 ease-in-out"
-                    src="/logo.svg"
-                    alt="Logo"
-                />
-                <Title className="-m-1 text-3xl" ta="center">
-                    Register your account
-                </Title>
-
-                <TextInput
-                    className="mt-20"
-                    variant="filled"
-                    label="User Name"
-                    placeholder="Your username"
-                    radius="md"
-                    required
-                    error=""
+        <Paper className='ml-10 p-20 w-full' shadow="lg" radius="lg">
+            <img className='mx-auto pr-4 mb-6 w-2/2 hover:brightness-75 cursor-pointer transition duration-400 ease-in-out' src="/logo.svg" alt="Logo" />
+            <Title className='-m-1 text-3xl' ta="center">
+                Register your account
+            </Title>
+            
+            <TextInput 
+                className='mt-11' 
+                variant='filled' 
+                label="User Name" 
+                placeholder="Your username" 
+                radius="md" 
+                required 
+                error=""
                     onChange={(e) => setUsername(e.target.value)}
-                    // TODO: Put error message here
-                />
+                // TODO: Put error message here
+            />
 
-                <TextInput
-                    variant="filled"
-                    label="Email"
-                    placeholder="Your email"
-                    radius="md"
-                    mt="md"
-                    required
-                    error=""
+            <TextInput 
+                variant='filled' 
+                label="Email" 
+                placeholder="Your email" 
+                radius="md" 
+                mt="md" 
+                required
+                error=""
                     onChange={(e) => setEmail(e.target.value)}
-                    // TODO: Put error message here
-                />
+                // TODO: Put error message here
+            />
 
-                <PasswordInput
-                    variant="filled"
-                    label="Password"
-                    placeholder="Your password"
-                    radius="md"
-                    mt="md"
-                    required
-                    error=""
-                    onChange={(e) => setPassowrd(e.target.value)}
-                    // TODO: Put error message here
-                />
+            <PasswordInput 
+                variant='filled' 
+                label="Password" 
+                placeholder="Your password" 
+                radius="md" 
+                mt="md" 
+                required
+                error=""
+                onChange={(e) => setPassowrd(e.target.value)}
+                // TODO: Put error message here
+            />
 
-                <PasswordInput
-                    variant="filled"
-                    label="Password"
-                    placeholder="Your password"
-                    radius="md"
-                    mt="md"
-                    required
-                    error=""
+            <PasswordInput 
+                variant='filled' 
+                label="Password" 
+                placeholder="Your password" 
+                radius="md" 
+                mt="md" 
+                required
+                error=""
                     onChange={(e) => setConfirm(e.target.value)}
-                    // TODO: Put error message here
-                />
-                {/* <Group justify="space-between" mt="lg">
-                <Checkbox label="I agree to the Privacy Policy"/>
-            </Group> */}
-
-                {/* BUG: variant: filled would not work at all AAAAAAAAAAAAAAAAAAAAAAAAAAAA */}
-                <div className="mt-14">
-                    <Button
-                        variant="outline"
-                        color="primary-blue"
-                        onClick={submit}
-                        fullWidth
-                    >
-                        Button
-                    </Button>
-                </div>
-
-                <Group mt="lg" className="justify-center">
-                    <p className="text-sm"> Have an account?</p>
-                    <Link
-                        to="/sign-in"
-                        className="text-sm font-bold -ml-3 text-primary-blue hover:underline decoration-2 cursor-pointer"
-                    >
-                        Sign in
-                    </Link>
+                // TODO: Put error message here
+            />
+            <Group justify="end" mt="lg">
+                <Checkbox/> 
+                    <p className='text-sm -ml-1'> I agree to the Privacy Policy </p>
                 </Group>
-            </Paper>
+
+            <div className='mt-14'>
+                <Button 
+                    onClick={submit}
+                    color="primary-blue" 
+                    fullWidth>
+                        Register
+                    </Button>
+            </div>
+
+            <Group  mt="lg" className='justify-center'>
+                <p className='text-sm'> Have an account?</p>
+                <Link to='/sign-in' className='text-sm font-bold -ml-3 text-primary-blue hover:underline decoration-2 cursor-pointer'>Sign in</Link>
+            </Group>
+        </Paper>
         </>
     )
 }
 
-export default RegisterForm
+export default RegisterForm;
--- frontend/src/components/sign-in/SignInForm.tsx
+++ frontend/src/components/sign-in/SignInForm.tsx
@@ -15,7 +15,7 @@ import { Link } from "react-router-dom"
 import useLogin from "../../hooks/useLogin"
 
 const SignInForm = () => {
-    const [username, setUsername] = useState<string>()
+const [username, setUsername] = useState<string>()
     const [password, setPassword] = useState<string>()
     const { login } = useLogin()
 
@@ -25,75 +25,64 @@ const SignInForm = () => {
 
     return (
         <>
-            <Paper className="h-full p-20 px-40" shadow="lg" radius="lg">
-                <img
-                    className="mx-auto pr-4 mb-12 w-2/3 hover:brightness-75 cursor-pointer transition duration-400 ease-in-out"
-                    src="/logo.svg"
-                    alt="Logo"
-                />
-                <Text className="text-lg" ta="center">
-                    Welcome back!
-                </Text>
-                <Title className="-m-1 text-3xl" ta="center">
-                    Sign in now
-                </Title>
 
-                <TextInput
-                    className="mt-20"
-                    variant="filled"
-                    label="User Name"
-                    placeholder="Your username"
-                    radius="md"
-                    required
-                    error=""
+        <Paper className='p-20 w-[100%] mr-10' shadow="lg" radius="lg">
+            <img className='mx-auto pr-4 mb-6 w-2/2 hover:brightness-75 cursor-pointer transition duration-400 ease-in-out' src="/logo.svg" alt="Logo" />
+            <Text className='text-lg' ta="center">
+                Welcome back!
+            </Text>
+            <Title className='-m-1 text-3xl' ta="center">
+                Sign in now
+            </Title> 
+
+            <TextInput 
+                className='mt-14' 
+                variant='filled' 
+                label="User Name" 
+                placeholder="Your username" 
+                radius="md" 
+                required 
+                error=""
                     onChange={(e) => setUsername(e.target.value)}
-                    // TODO: Put error message here
-                />
+                // TODO: Put error message here
+            />
 
-                <PasswordInput
-                    variant="filled"
-                    label="Password"
-                    placeholder="Your password"
-                    radius="md"
-                    mt="md"
-                    required
-                    error=""
+            <PasswordInput 
+                variant='filled' 
+                label="Password" 
+                placeholder="Your password" 
+                radius="md" 
+                mt="md" 
+                required
+                error=""
                     onChange={(e) => setPassword(e.target.value)}
-                    // TODO: Put error message here
-                />
-
-                <Group justify="space-between" mt="lg">
-                    {/* BUG: checkbox label nasa right even if set with prop sa left AAAAAAAAA BOBO MO MANTINE */}
-                    <Checkbox label="Remember me" />
-                    <Anchor component="button" size="sm">
-                        Forgot password?
-                    </Anchor>
-                </Group>
-
-                {/* BUG: variant: filled would not work at all AAAAAAAAAAAAAAAAAAAAAAAAAAAA */}
-                <div className="mt-20">
-                    <Button
-                        onClick={submit}
-                        variant="outline"
-                        color="primary-blue"
-                        fullWidth
-                    >
-                        Button
-                    </Button>
+                // TODO: Put error message here
+            />
+            
+            <Group justify="space-between" mt="lg">
+                {/* BUG: checkbox label nasa right even if set with prop sa left AAAAAAAAA BOBO MO MANTINE */}
+                <div className='flex'>
+                    <Checkbox className='mr-2'/> 
+                    <p className='text-sm'> Remember me </p>
                 </div>
+            </Group>
 
-                <Group mt="lg" className="justify-center">
-                    <p className="text-sm"> Not a member?</p>
-                    <Link
-                        to="/register"
-                        className="text-sm font-bold -ml-2 text-primary-blue hover:underline decoration-2 cursor-pointer"
-                    >
-                        Register now
-                    </Link>
-                </Group>
-            </Paper>
+            <div className='mt-20'>
+                <Button 
+                    onClick={submit}
+                    color="primary-blue" 
+                    fullWidth>Sign in
+                </Button>
+            </div>
+
+            <Group  mt="lg" className='justify-center'>
+                <p className='text-sm'>Not a member?</p>
+                <Link to='/register' className='text-sm font-bold -ml-2 text-primary-blue hover:underline decoration-2 cursor-pointer'>Register now</Link>
+            </Group>
+            
+        </Paper>
         </>
     )
 }
 
-export default SignInForm
+export default SignInForm;
