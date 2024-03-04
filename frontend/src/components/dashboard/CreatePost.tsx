8b32ab7a0947a069fbcf61cc352a1c160d67aa7a
anaj00 <bantolino.jana@gmail.com>
2024-03-04 10:10:34 +0800

    feat: added insert photos to post

--- frontend/src/components/dashboard/CreatePost.tsx
+++ frontend/src/components/dashboard/CreatePost.tsx
@@ -1,7 +1,12 @@
 import {
   ActionIcon, 
-  Button
+  Button,
+  FileButton,
+  Group, 
+  Text
 } from '@mantine/core';
+
+import { useState } from 'react';
 
 import { RichTextEditor, Link } from '@mantine/tiptap';
 import { useEditor } from '@tiptap/react';
@@ -15,10 +20,6 @@ import { IconColorPicker } from '@tabler/icons-react';
 import { Color } from '@tiptap/extension-color';
 import TextStyle from '@tiptap/extension-text-style';
 import Placeholder from '@tiptap/extension-placeholder';
-
-import {
-  Textarea,
-} from '@mantine/core';
 
 import { PiImageBold, PiArrowRightBold } from "react-icons/pi";
 
@@ -41,6 +42,8 @@ const CreatePost = () => {
     content,
   });
 
+  const [file, setFile] = useState<File | null>(null);
+
   return (
     <div className="bg-white w-full h-auto rounded-2xl p-6 mb-8 shadow-md">
       <div className='flex space-x-6'>
@@ -50,85 +53,104 @@ const CreatePost = () => {
           </div>
         </div>
 
-        <RichTextEditor className="w-full pt-1" editor={editor}>
-          <RichTextEditor.Toolbar sticky stickyOffset={60}>
-            <RichTextEditor.ColorPicker
-              colors={[
-                '#25262b',
-                '#868e96',
-                '#fa5252',
-                '#e64980',
-                '#be4bdb',
-                '#7950f2',
-                '#4c6ef5',
-                '#228be6',
-                '#15aabf',
-                '#12b886',
-                '#40c057',
-                '#82c91e',
-                '#fab005',
-                '#fd7e14',
-              ]}
-            />
+        <div>
+          <RichTextEditor className="w-full pt-1" editor={editor}>
+            <RichTextEditor.Toolbar sticky stickyOffset={60}>
+              <RichTextEditor.ColorPicker
+                colors={[
+                  '#25262b',
+                  '#868e96',
+                  '#fa5252',
+                  '#e64980',
+                  '#be4bdb',
+                  '#7950f2',
+                  '#4c6ef5',
+                  '#228be6',
+                  '#15aabf',
+                  '#12b886',
+                  '#40c057',
+                  '#82c91e',
+                  '#fab005',
+                  '#fd7e14',
+                ]}
+              />
 
-            <RichTextEditor.ControlsGroup>
-              <RichTextEditor.Control interactive={false}>
-                <IconColorPicker size="1rem" stroke={1.5} />
-              </RichTextEditor.Control>
-              <RichTextEditor.Color color="#F03E3E" />
-              <RichTextEditor.Color color="#7048E8" />
-              <RichTextEditor.Color color="#1098AD" />
-              <RichTextEditor.Color color="#37B24D" />
-              <RichTextEditor.Color color="#F59F00" />
-              <RichTextEditor.UnsetColor />
-            </RichTextEditor.ControlsGroup>
+              <RichTextEditor.ControlsGroup>
+                <RichTextEditor.Control interactive={false}>
+                  <IconColorPicker size="1rem" stroke={1.5} />
+                </RichTextEditor.Control>
+                <RichTextEditor.Color color="#F03E3E" />
+                <RichTextEditor.Color color="#7048E8" />
+                <RichTextEditor.Color color="#1098AD" />
+                <RichTextEditor.Color color="#37B24D" />
+                <RichTextEditor.Color color="#F59F00" />
+                <RichTextEditor.UnsetColor />
+              </RichTextEditor.ControlsGroup>
 
-            <RichTextEditor.ControlsGroup>
-              <RichTextEditor.Bold />
-              <RichTextEditor.Italic />
-              <RichTextEditor.Underline />
-              <RichTextEditor.Strikethrough />
-              <RichTextEditor.ClearFormatting />
-              <RichTextEditor.Highlight />
-              <RichTextEditor.Code />
-            </RichTextEditor.ControlsGroup>
+              <RichTextEditor.ControlsGroup>
+                <RichTextEditor.Bold />
+                <RichTextEditor.Italic />
+                <RichTextEditor.Underline />
+                <RichTextEditor.Strikethrough />
+                <RichTextEditor.ClearFormatting />
+                <RichTextEditor.Highlight />
+                <RichTextEditor.Code />
+              </RichTextEditor.ControlsGroup>
 
-            <RichTextEditor.ControlsGroup>
-              <RichTextEditor.H1 />
-              <RichTextEditor.H2 />
-              <RichTextEditor.H3 />
-              <RichTextEditor.H4 />
-            </RichTextEditor.ControlsGroup>
+              <RichTextEditor.ControlsGroup>
+                <RichTextEditor.H1 />
+                <RichTextEditor.H2 />
+                <RichTextEditor.H3 />
+                <RichTextEditor.H4 />
+              </RichTextEditor.ControlsGroup>
 
-            <RichTextEditor.ControlsGroup>
-              <RichTextEditor.Blockquote />
-              <RichTextEditor.Hr />
-              <RichTextEditor.BulletList />
-              <RichTextEditor.OrderedList />
-              <RichTextEditor.Subscript />
-              <RichTextEditor.Superscript />
-            </RichTextEditor.ControlsGroup>
+              <RichTextEditor.ControlsGroup>
+                <RichTextEditor.Blockquote />
+                <RichTextEditor.Hr />
+                <RichTextEditor.BulletList />
+                <RichTextEditor.OrderedList />
+                <RichTextEditor.Subscript />
+                <RichTextEditor.Superscript />
+              </RichTextEditor.ControlsGroup>
 
-            <RichTextEditor.ControlsGroup>
-              <RichTextEditor.Link />
-              <RichTextEditor.Unlink />
-            </RichTextEditor.ControlsGroup>
+              <RichTextEditor.ControlsGroup>
+                <RichTextEditor.Link />
+                <RichTextEditor.Unlink />
+              </RichTextEditor.ControlsGroup>
 
-            <RichTextEditor.ControlsGroup>
-              <RichTextEditor.AlignLeft />
-              <RichTextEditor.AlignCenter />
-              <RichTextEditor.AlignJustify />
-              <RichTextEditor.AlignRight />
-            </RichTextEditor.ControlsGroup>
+              <RichTextEditor.ControlsGroup>
+                <RichTextEditor.AlignLeft />
+                <RichTextEditor.AlignCenter />
+                <RichTextEditor.AlignJustify />
+                <RichTextEditor.AlignRight />
+              </RichTextEditor.ControlsGroup>
 
-            <RichTextEditor.ControlsGroup>
-              <RichTextEditor.Undo />
-              <RichTextEditor.Redo />
-            </RichTextEditor.ControlsGroup>
-          </RichTextEditor.Toolbar>
+              <RichTextEditor.ControlsGroup>
+                <RichTextEditor.Undo />
+                <RichTextEditor.Redo />
+              </RichTextEditor.ControlsGroup>
+            </RichTextEditor.Toolbar>
 
-          <RichTextEditor.Content />
-        </RichTextEditor>
+            <RichTextEditor.Content />
+          </RichTextEditor>
+
+          <div className="flex flex-row text-primary-blue justify-between items-end">
+          
+            <div className='-mb-1'>
+              <FileButton onChange={setFile} accept="image/png,image/jpeg">
+                  {(props) => 
+                    <ActionIcon {...props} radius='xl' size='xl'>
+                      <PiImageBold size={20} />
+                    </ActionIcon> }
+              </FileButton>
+            </div>
+
+            <Button className='rounded-2xl hover:bg-black mt-4 duration-300 ease-in-out'>
+              Post
+            </Button>
+          
+          </div>
+        </div>
       </div>
 
       {/* <Textarea
@@ -140,21 +162,7 @@ const CreatePost = () => {
                 className="w-full pt-1"
             /> */}
 
-      <div className="flex flex-row text-primary-blue justify-end">
-        {/* TODO: Add dropzone */}
-        <div>
-          {/* TODO: Add image + video input */}
-          {/* <ActionIcon radius='xl' size='xl'>
-            <PiImageBold size={20} />
-          </ActionIcon> */}
-
-          <Button className='rounded-2xl hover:bg-black mt-4 duration-300 ease-in-out'>
-            Post
-          </Button>
-        </div>
-        
-        
-      </div>
+      
 
     </div>
   );
