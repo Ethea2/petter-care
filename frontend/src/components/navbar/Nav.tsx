b78c1ebda11521da30193e71b6336a581177ce21
anaj00 <bantolino.jana@gmail.com>
2024-04-02 19:43:55 +0800

    chore: added edit to navbar

--- frontend/src/components/navbar/Nav.tsx
+++ frontend/src/components/navbar/Nav.tsx
@@ -1,22 +1,45 @@
-// import { useState } from "react"
-import { rem, Menu } from "@mantine/core"
+import { useDisclosure } from '@mantine/hooks';
+import { rem, Menu, Modal, Button, Group, TextInput, Textarea, PasswordInput } from "@mantine/core"
 
 import {
     IconLogout,
     IconSettings,
-    IconMessageCircle
 } from "@tabler/icons-react"
 
 import UserButton from "./UserButton"
 import { Link } from "react-router-dom"
-// import { AiFillCaretDown } from "react-icons/ai"
-import { PiHouseFill, PiBellFill } from "react-icons/pi"
+import { PiHouseFill, PiDogFill, PiCatFill } from "react-icons/pi"
 
 const Nav = () => {
+    const [opened, { open, close }] = useDisclosure(false);
+
     return (
-        <>
+        <>  
+            <Modal opened={opened} onClose={close} title="Edit profile" centered>
+                <TextInput
+                    className='mb-3'
+                    label="User Name"
+                    placeholder="Your username"
+                />
+
+                <Textarea
+                    className='mb-3'
+                    label="Bio"
+                    placeholder="Your bio"
+                />
+
+
+                <div className="mt-4 flex justify-end">
+                    <Button
+                        className="rounded-2xl duration-300 ease-in-out"
+                        color="primary-blue"
+                    >
+                        Save
+                    </Button>
+                </div>
+            </Modal>
+
             <div className="sticky top-0 flex items-center w-full h-auto bg-dirty-white py-3 drop-shadow-md z-50 justify-between">
-                {/* Logo and Search*/}
                 <div className="flex w-95">
                     <a
                         className="w-60 h-auto pl-4 pr-6 hover:brightness-75 cursor-pointer transition duration-400 ease-in-out"
@@ -26,11 +49,19 @@ const Nav = () => {
                     </a>
                 </div>
 
-                {/* Right */}
-
                 <div className="flex items-center space-x-6 text-3xl text-primary-blue mr-3">
-                    <PiHouseFill className="hover:brightness-75 cursor-pointer transition duration-400 ease-in-out" />
-                    <PiBellFill className="hover:brightness-75 cursor-pointer transition duration-400 ease-in-out" />
+                    <Link to="/dashboard">
+                        <PiHouseFill 
+                            className="hover:brightness-75 cursor-pointer transition duration-400 ease-in-out" />
+                    </Link>
+
+                    <Link to="/pet-access"
+                        className="flex hover:brightness-75 cursor-pointer transition duration-400 ease-in-out"
+                        >
+                        <PiDogFill />
+                        <PiCatFill />
+                    </Link>
+                    
 
                     <div className="flex items-center -mt-2">
                         <Menu trigger="hover" position="bottom-end">
@@ -46,7 +77,7 @@ const Nav = () => {
                             </Menu.Target>
 
                             <Menu.Dropdown>
-                                <Menu.Item
+                                <Menu.Item onClick={open}
                                     leftSection={
                                         <IconSettings
                                             style={{
@@ -56,21 +87,9 @@ const Nav = () => {
                                         />
                                     }
                                 >
-                                    Settings
+                                    Edit Profile
                                 </Menu.Item>
 
-                                <Menu.Item
-                                    leftSection={
-                                        <IconMessageCircle
-                                            style={{
-                                                width: rem(14),
-                                                height: rem(14)
-                                            }}
-                                        />
-                                    }
-                                >
-                                    Messages
-                                </Menu.Item>
 
                                 <Menu.Divider />
 
