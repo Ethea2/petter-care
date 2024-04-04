df5b732af0036e0740d7b082daa4d7753e9de328
Ethea2 <nathandres2001@gmail.com>
2024-04-05 02:28:30 +0800

    added: a quick fix for deployment

--- frontend/src/pages/ViewPost.tsx
+++ frontend/src/pages/ViewPost.tsx
@@ -1,25 +1,24 @@
-import ProfileCard from '../components/profile/ProfileCard.tsx'
-import Post from '../components/dashboard/Post.tsx'
+import ProfileCard from "../components/profile/ProfileCard.tsx"
+import Post from "../components/dashboard/Post.tsx"
+import { IUser } from "../types/userTypes.tsx"
 
 const UserProf = () => {
+    const user = {} as IUser
     return (
         <>
             <div className="min-h-screen flex px-40 py-10 bg-gradient-to-r from-primary-blue to-accent3-purple">
                 <div className="lg:w-[25%] xl:w-[25%] 2xl:w-[25%] space-y-10">
-                    <ProfileCard />
+                    <ProfileCard user={user} />
                 </div>
 
                 <div className="lg:w-[75%] xl:w-[70%] 2xl:w-[80%] pl-12 -mt-6">
-                    <Post/>
+                    <Post />
 
                     <div>
-                        <Post/>
+                        <Post />
                     </div>
                 </div>
-            </div >
-
-
-                
+            </div>
         </>
     )
 }
